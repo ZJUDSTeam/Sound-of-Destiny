ebbd3c89818c64d7657c01310f8fb2af3cb6863b
zht <3170102473@zju.edu.cn>
2018-10-25 23:37:26 +0800

    ...first version!

--- BuildBST.c
+++ BuildBST.c
@@ -3,27 +3,36 @@
 //
 #include "BuildBST.h"
 
+/*
+ * this function will read the relationship between nodes
+ * then build a tree with initial value INF
+ */
 Tree BuildTree(int N){
-    int index;
+    int index; /* the index of current node */
     Tree T;
-    T = (Tree)calloc(sizeof(struct TreeNode),(N));
+    T = (Tree)calloc(sizeof(struct TreeNode),(N)); /* initialize the tree */
     if (T==NULL){
         printf("Error! Can't allocate memory!\n");
         return 0;
     }
+    /* read the parent-child relationship and build the tree */
     for (index=0; index<N; index++){
+        /* get the index of left child and right child */
         int left_index, right_index;
         scanf("%d%d", &left_index,&right_index);
         T[index].left = left_index;
         T[index].right = right_index;
-        T[index].value = INF;
+        T[index].value = INF; /* set the initial value */
     }
     return T;
 }
 
+/*
+ * the function GetKey will read the given keys
+ */
 int* GetKey(int N){
     int* K;
-    K = (int*)calloc(sizeof(int),(N));
+    K = (int*)calloc(sizeof(int),(N)); /* initialize the key array */
     int i;
     for (i=0; i<N; i++){
         scanf("%d",&K[i]);
@@ -31,6 +40,10 @@ int* GetKey(int N){
     return K;
 }
 
+/*
+ * the function SortKey use Bubble Sort algorithm to sort the key
+ * the keys will be sorted in ascending order
+ */
 void SortKey(int* A, int N){
     int i,j,temp;
     for (i=0; i<N-1; i++)
@@ -42,6 +55,12 @@ void SortKey(int* A, int N){
             }
 }
 
+/*
+ * according to the characteristic of the Binary Search Tree
+ * its in-order traversal is an ascending sequence
+ * thus, we can insert the keys into the tree while do in-order traversal
+ * then a Binary Search Tree is build
+ */
 void InOrderInsert(Tree T, int root){
     if (T[root].left != Empty){
         InOrderInsert(T, T[root].left);
@@ -52,22 +71,29 @@ void InOrderInsert(Tree T, int root){
         InOrderInsert(T, T[root].right);
     }
 }
-
+/*
+ * level-order traversal by using queue
+ * first enqueue the root node
+ * while queue is not empty, dequeue the node and then enqueue its children
+ */
 void LevelOrder(Tree T){
     struct TreeNode queue[MaxTree], p;
+    /* the head points to the front of the queue
+     * the tail points to the rear of the queue
+     */
     int head = 0, tail = 1;
     queue[0] = T[0];
-    while (head != tail){
-        p = queue[head];
-        head = (head+1) % MaxTree;
-        printf("%d ", p.value);
-        if (p.left != Empty){
+    while (head != tail){ /* the queue is not empty */
+        p = queue[head]; /* get the head of queue */
+        head = (head+1) % MaxTree; /* update the head */
+        printf("%d ", p.value); /* visit the node */
+        if (p.left != Empty){ /* if the current node has left children, let it enqueue */
             queue[tail] = T[p.left];
-            tail = (tail+1) % MaxTree;
+            tail = (tail+1) % MaxTree; /* prevent array subscripts from crossing the boundary  */
         }
-        if (p.right != Empty){
+        if (p.right != Empty){ /* if the current node has right children, let it enqueue */
             queue[tail] = T[p.right];
-            tail = (tail+1) % MaxTree;
+            tail = (tail+1) % MaxTree; /* prevent array subscripts from crossing the boundary  */
         }
     }
 }
