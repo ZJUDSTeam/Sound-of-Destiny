29c0e90d8f4a356f25a934444426b852b503b346
zht <3170102473@zju.edu.cn>
2018-10-26 01:05:25 +0800

    ...

--- main.c
+++ main.c
@@ -1,9 +1,8 @@
 //
 // Created by 59932 on 2018/10/23.
 //
-<<<<<<< HEAD
-#include "BuildBST.c"
 
+#include "BuildBST.c"
 
 int main(void)
 {
@@ -16,28 +15,4 @@ int main(void)
     InOrderInsert(T,0);
     LevelOrder(T);
 }
-=======
-int foo(int n);
-#include <stdio.h>
-int main(void)
-{
-    printf("%d",foo(10));
 
-}
-
-
-int foo(int n)
-{
-    static int a = 0;
-    if( n != 0 )
-    {
-        a++;
-        foo(n-1);
-    }
-    return a;
-}
-
-
-
-
->>>>>>> origin/master
